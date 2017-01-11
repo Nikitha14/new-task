e1be196cff096a00dffeb3b7c65cf8dd91627e3e
Nikitha14 <nikitha.nitwarangal@gmail.com>
2017-01-11 16:06:27 -0500

    initial commit

---
+++ contactweb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(contactweb.Startup))]
+namespace contactweb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
